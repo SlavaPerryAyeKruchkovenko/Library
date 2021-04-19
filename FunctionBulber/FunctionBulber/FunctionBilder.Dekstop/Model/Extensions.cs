03e85ba08c48e595b7f4b5bcaa357b08440e172e
Slava <sh-e-95@mail.ru>
2021-04-19 23:01:17 +0500

    Main

---
+++ FunctionBulber/FunctionBulber/FunctionBilder.Dekstop/Model/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Linq;
+
+namespace FunctionBilder.Dekstop.Model
+{
+	static class DoubleExtensions
+	{
+		public static int Length(this double num)
+		{
+			double num2 = (int)num;
+			if (num - num2 == Math.Round(num - num2))
+				return 0;
+			string s = (num - num2).ToString();
+			return s.Length-2;
+		}
+	}
+	static class DoubleArrayExtensions
+	{
+		public static double[] Mul(this double[] nums, double num)
+		{
+			double[] countNums = new double[nums.Length];
+			for (int i = 0; i < nums.Length; i++)
+			{
+				countNums[i] = nums[i] * num;
+			}
+			return countNums;
+		}
+	}
+	static class TextboxArrayExtension
+	{
+		public static double[] ToDouble(this TextBox[] boxes)
+		{
+			return new double[]
+			{
+				Convert.ToDouble(boxes[0].Text),
+				Convert.ToDouble(boxes[1].Text),
+				Convert.ToDouble(boxes[2].Text)
+			};
+		}
+	}
+}
---
+++ FunctionBulber/FunctionBulber/FunctionBilder.Dekstop/ViewModel/Function.cs
@@ -0,0 +1,65 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using System.Collections.Generic;
+
+namespace FunctionBilder.Dekstop.ViewModel
+{
+	class Function
+	{
+		private IFunctionDrawer functionDrawer { get; }
+		private Point rangeLocation { get; }
+		private Point layoutSize { get; }
+		private IBrush[] brushes { get; }
+		public Function(IFunctionDrawer _functionDrawer, Point _rangeLocation, Point _canvasSize, IBrush[] _brushes)
+		{
+			functionDrawer = _functionDrawer;
+			rangeLocation = _rangeLocation;
+			layoutSize = _canvasSize;
+			brushes = _brushes;
+		}
+		public List<Point> Render(Point cycleSize, string function, double range, double scale)
+		{
+			Point[] points = new Point[]
+			{
+				new Point(-layoutSize.X/2-rangeLocation.X,layoutSize.X/2-rangeLocation.X),
+				new Point(layoutSize.X/2,layoutSize.Y/2)+rangeLocation
+			};
+			functionDrawer.DrawLabels(points[0], points[1], scale, true);
+
+			points = new Point[]
+			{
+				new Point(-layoutSize.Y/2+rangeLocation.Y,layoutSize.Y/2+rangeLocation.Y),
+				new Point(layoutSize.X/2,layoutSize.Y/2)+rangeLocation
+			};
+			functionDrawer.DrawLabels(points[0], points[1], scale, false);
+
+			points = new Point[]
+			{
+				new Point(0, layoutSize.Y / 2+rangeLocation.Y),
+				new Point(layoutSize.X, layoutSize.Y / 2+rangeLocation.Y)
+			};
+
+			functionDrawer.DrawLine(points[0], points[1], Brushes.DeepPink);
+
+			points = new Point[]
+			{
+				new Point(layoutSize.X / 2+rangeLocation.X, 0),
+				new Point(layoutSize.X / 2+rangeLocation.X, layoutSize.Y)
+			};
+
+			functionDrawer.DrawLine(points[0], points[1], Brushes.DeepPink);
+
+			points = new Point[]
+			{
+				new Point(layoutSize.X, layoutSize.Y / 2+rangeLocation.Y),
+				new Point(layoutSize.X / 2+rangeLocation.X, 0)
+			};
+
+			functionDrawer.DrawArrows(points[0], new Point(10, 10));
+
+			functionDrawer.DrawArrows(points[1], new Point(10, -10));
+
+			return functionDrawer.DrawFunction(cycleSize, range, function, rangeLocation, scale, brushes);
+		}
+	}
+}
