2523f29db4d3cd0bfe44dfc848165f3789b6df28
Slava <sh-e-95@mail.ru>
2021-05-01 00:26:44 +0500

    В рот я ебал бидинг 2

--- FunctionBulber/FunctionBilder.Dekstop/View/GraphicWindow.axaml.cs
+++ FunctionBulber/FunctionBilder.Dekstop/View/GraphicWindow.axaml.cs
@@ -1,11 +1,13 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using FunctionBilder.Dekstop.Model;
 using FunctionBilder.Dekstop.ViewModel;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace FunctionBilder.Dekstop.View
 {
@@ -14,14 +16,17 @@ namespace FunctionBilder.Dekstop.View
 		private TextBox inputBox { get; }
 		private TextBox[] rangeBoxes { get; }
 		private Function function { get; set; }
-
-		private static readonly List<IBrush> colors = new List<IBrush>() {Brushes.Aqua,Brushes.Purple,Brushes.Sienna,Brushes.Silver,Brushes.SkyBlue,Brushes.White,Brushes.YellowGreen };
+		private IBrush color { get; set; }
+		public ObservableCollection<IBrush> colors { get; } 
 		public GraphicWindow()
 		{
+			this.colors = new ObservableCollection<IBrush>() { Brushes.Aqua, Brushes.Purple, Brushes.Sienna, Brushes.Silver, Brushes.SkyBlue, Brushes.White, Brushes.YellowGreen };
 			InitializeComponent();
 #if DEBUG
 			this.AttachDevTools();
 #endif
+			var menu = this.FindControl<ContextMenu>("ColorMenu");
+			menu.Items = this.colors;
 			this.inputBox = this.FindControl<UserControl>("InputBox").FindControl<TextBox>("FunctuionBox");
 			this.rangeBoxes = MainWindow.FoundTextBoxs(this);
 		}
@@ -29,8 +34,16 @@ namespace FunctionBilder.Dekstop.View
 		{
 			AvaloniaXamlLoader.Load(this);
 		}
-		private void AddFunction(object sender, RoutedEventArgs e)
+		private void AddColor(object sender, RoutedEventArgs e)
 		{
+			this.color = (IBrush)((MenuItem)sender);
+		}
+		private void OpenMenu(object sender, RoutedEventArgs e)
+		{
+			((MenuItem)sender).ContextMenu.Open();
+		}
+		private void AddFunction(object sender, RoutedEventArgs e)
+		{			
 			var graphic = new Graphic(this.rangeBoxes.ToDouble());
 			this.function = new Function(this.inputBox.Text, graphic);
 			this.Close();
